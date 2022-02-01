72f17e7602567468c2919683f836671daddabee8
PCristian00 <cristianp9300@gmail.com>
2022-02-01 18:59:48 +0100

    Stampa con errore

--- main.cpp
+++ main.cpp
@@ -5,60 +5,59 @@
 using namespace std;
 
 struct bmi{
-    char desc[30];
+    char desc[12];
     float value;
 };
 
-char *Calcola(float h, float w){
-    char out[50];
+static char *Calcola(float h, float w){
+    char* msg[50];
     bmi b{};
 
     b.value=(float) w/(h*h);
     //cout<<b.value<<endl;
 
-    if(b.value<18.5) strcpy(b.desc,"\tSottopeso");
-    else if(b.value>25) strcpy(b.desc,"\tSovrappeso");
-    else strcpy(b.desc,"\tNormale");
+    if(b.value<18.5) strcpy(b.desc," Sottopeso");
+    else if(b.value>25) strcpy(b.desc," Sovrappeso");
+    else strcpy(b.desc," Normale");
 
-    snprintf(out, 50, "%.1f", b.value);
-    //cout<<out<<endl;
+    snprintf(*msg, sizeof msg, "%.1f", b.value);
+    //cout<<msg<<endl;
     //value=(b.value);
-    strcat(out,b.desc);
-    //cout<<"Sono nella funzione e il tuo BMI e' "<<endl;
-    //cout<<out;
+    strcat(*msg, b.desc);
+   /*
+    cout<<"Sono nella funzione e il tuo BMI e' "<<endl;
+    cout<<msg<<endl;
+    */
+   //cout<<*msg;
 
-    return out;
+    return *msg;
 }
 
 int main() {
 
     fstream h,w,out;
     float hd[10],wd[10];
-
-    h.open("C:\\Users\\Thinkpad User\\CLionProjects\\bmi\\altezza.txt",ios::in);
-    if(!h.is_open()) cout<<"Errore apertura altezze."<<endl;
-
-    w.open("C:\\Users\\Thinkpad User\\CLionProjects\\bmi\\peso.txt",ios::in);
-    if(!w.is_open()) cout<<"Errore apertura peso."<<endl;
-
-    out.open("C:\\Users\\Thinkpad User\\CLionProjects\\bmi\\BMI.txt",ios::out);
-    if(!out.is_open()) cout<<"Errore apertura BMI."<<endl;
-
-    out<<"\tBMI\t"<<endl;
-
     int i=0;
     char *line;
-    while(i<2){
-        cout<<"Hola"<<endl;
+
+    h.open(R"(C:\Users\Thinkpad User\CLionProjects\bmi\altezza.txt)",ios::in);
+    if(!h.is_open()) cout<<"Errore apertura altezze."<<endl;
+
+    w.open(R"(C:\Users\Thinkpad User\CLionProjects\bmi\peso.txt)",ios::in);
+    if(!w.is_open()) cout<<"Errore apertura peso."<<endl;
+
+    out.open(R"(C:\Users\Thinkpad User\CLionProjects\bmi\BMI.txt)",ios::out);
+    if(!out.is_open()) cout<<"Errore apertura BMI."<<endl;
+
+    out<<"BMI"<<endl;
+    //cout<<Calcola(2.3,4.5)<<endl;
+    while(w&&h){
+        //cout<<"Hola\t"<<i<<endl;
         h>>hd[i];
-        if(hd[i]==0) cout<<"ehi che fai";
         w>>wd[i];
-        if(wd[i]==0) cout<<"ehi che fai pesa poco";
-        //TODO non fa nulla
-        //cout<<Calcola(hd[i],wd[i]);
         line=Calcola(hd[i],wd[i]);
-        //cout<<line;
-        out<<i<<".\t"<<line<<endl;
+        out<<line<<endl;
+        cout<<line<<endl;
         i++;
     }
     return 0;
